965bb501e0beac026815f617f3266288d0c724f2
Vicente <vicente@comunitea.com>
2026-03-09 14:45:18 +0100

    [UPD] Actualización para evitar caducidad de cuenta de Google Play

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'comunitea.reg.clock',
   appName: 'control-de-fichaje',
   webDir: 'www',
   server: {
